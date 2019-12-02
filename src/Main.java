697c1a25078de522fa63bc1d5abb40612435c0a3
yanyichi <yanyichi143@hotmail.com>
2019-12-02 14:50:43 -0800

    repo for summit

--- src/Main.java
+++ src/Main.java
@@ -44,42 +44,57 @@ public class Main {
         assignment3.addAssignmentScore(student1,60);
         assignment4.addAssignmentScore(student1,85);
         assignment5.addAssignmentScore(student1,92);
-//
-//        assignment1.addAssignmentScore(student2,80);
-//        assignment2.addAssignmentScore(student2,87);
-//        assignment3.addAssignmentScore(student2,76);
-//        assignment4.addAssignmentScore(student2,81);
-//        assignment5.addAssignmentScore(student2,91);
-//
-//        assignment1.addAssignmentScore(student3,70);
-//        assignment2.addAssignmentScore(student3,85);
-//        assignment3.addAssignmentScore(student3,90);
-//        assignment4.addAssignmentScore(student3,95);
-//        assignment5.addAssignmentScore(student3,88);
-
 
         exam1.addStudentExamScore(student1,94);
         exam2.addStudentExamScore(student1,99);
         exam3.addStudentExamScore(student1,91);
-//
-//        exam1.addStudentExamScore(student2,81);
-//        exam2.addStudentExamScore(student2,88);
-//        exam3.addStudentExamScore(student2,95);
-//
-//
-//        exam1.addStudentExamScore(student3,70);
-//        exam2.addStudentExamScore(student3,75);
-//        exam3.addStudentExamScore(student3,88);
 
         //(90+88+60+85+92)/5*0.4  + (94+99+91)/3*0.6 = 90.0
         System.out.println(GradeTool.gradeToolSingleton().findStudentRosterGrade(student1,roster));
         //(90+88+85+92)/4*0.4  + (94+99+91)/3*0.6 = 92.3
         System.out.println(GradeTool.gradeToolSingleton().findStudentRosterGradeWithDropAssignment(student1,roster,1));
 
+        System.out.println(GradeTool.gradeToolSingleton().getFinalGrade(student1,roster));
+
+       assignment1.addAssignmentScore(student2,80);
+       assignment2.addAssignmentScore(student2,87);
+       assignment3.addAssignmentScore(student2,76);
+       assignment4.addAssignmentScore(student2,81);
+       assignment5.addAssignmentScore(student2,91);
+
+       exam1.addStudentExamScore(student2,81);
+       exam2.addStudentExamScore(student2,88);
+       exam3.addStudentExamScore(student2,95);
+
+
+        System.out.println(GradeTool.gradeToolSingleton().findStudentRosterGrade(student2,roster));
+
+        System.out.println(GradeTool.gradeToolSingleton().findStudentRosterGradeWithDropAssignment(student2,roster,1));
+
+        System.out.println(GradeTool.gradeToolSingleton().getFinalGrade(student2,roster));
+
+       assignment1.addAssignmentScore(student3,70);
+       assignment2.addAssignmentScore(student3,85);
+       assignment3.addAssignmentScore(student3,90);
+       assignment4.addAssignmentScore(student3,95);
+       assignment5.addAssignmentScore(student3,88);
+
+        exam1.addStudentExamScore(student3,70);
+        exam2.addStudentExamScore(student3,75);
+        exam3.addStudentExamScore(student3,88);
+
+
+        System.out.println(GradeTool.gradeToolSingleton().findStudentRosterGrade(student3,roster));
+
+        System.out.println(GradeTool.gradeToolSingleton().findStudentRosterGradeWithDropAssignment(student3,roster,1));
+
+        System.out.println(GradeTool.gradeToolSingleton().getFinalGrade(student3,roster));
+
+
         System.out.println(GradeTool.gradeToolSingleton().findRosterAvg(roster));
 
 
-        System.out.println(GradeTool.gradeToolSingleton().getFinalGrade(student1,roster));
+
 
     }
 }
